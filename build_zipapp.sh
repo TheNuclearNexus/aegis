46717eb9cfb9d18063ae1221122f449c7b1bacc0
Yavanni <thenuclearnexus@gmail.com>
2026-01-17 20:48:05 -0500

    fix: bad import

--- build_zipapp.sh
+++ build_zipapp.sh
@@ -8,8 +8,8 @@ rm -rf build/*
 # cp -r language_server/* build/language_server
 
 
-pip install . --target build
+uv run pip install . --target build
 echo "from aegis_server.__main__ import main; main()" > build/__main__.py
 
-python -m zipapp build -o ../aegis-vscode/language_server.pyz
+uv run python -m zipapp build -o ../aegis-vscode/language_server.pyz
 cp ../aegis-vscode/language_server.pyz ../aegis-jetbrains/src/main/resources/language_server.pyz
