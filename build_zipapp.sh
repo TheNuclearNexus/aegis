5ff46b047ae4cf99f3065fb0cc64c64500a84852
Yavanni <thenuclearnexus@gmail.com>
2026-01-17 20:54:17 -0500

    fix: uv for zipapp

--- build_zipapp.sh
+++ build_zipapp.sh
@@ -8,8 +8,8 @@ rm -rf build/*
 # cp -r language_server/* build/language_server
 
 
-uv run pip install . --target build
+uv pip install . --target build
 echo "from aegis_server.__main__ import main; main()" > build/__main__.py
 
-uv run python -m zipapp build -o ../aegis-vscode/language_server.pyz
+uv run -m zipapp build -o ../aegis-vscode/language_server.pyz
 cp ../aegis-vscode/language_server.pyz ../aegis-jetbrains/src/main/resources/language_server.pyz
